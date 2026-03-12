e540a486fa21c17dbc7c9f4e0fb6dd1395fc9ea3
Luke Tyson <lukeool76@gmail.com>
2026-03-12 15:50:19 +0000

    fetch grid for gridSize

--- src/pages/ResultVisualization/ResultsVisualization.tsx
+++ src/pages/ResultVisualization/ResultsVisualization.tsx
@@ -4,30 +4,43 @@ import DataGrid from "./components/DataGrid/DataGrid.tsx";
 import CurrentPlaybackContextProvider from "./store/CurrentPlaybackContextProvider.tsx";
 import SelectedUnitContextProvider from "./store/SelectedUnitContextProvider.tsx";
 import PlaybackControls from "./components/PlaybackControls/PlaybackControls.tsx";
-import {useGridDataQuery} from "./components/service/useGridDataQuery.ts";
+import {useGridDataQuery, useGridQuery} from "./components/service/useGridDataQuery.ts";
+import {useContext} from "react";
+import {SelectedUnitContext} from "./store/SelectedUnitContext.ts";
 
 const ResultsVisualization: React.FC = () => {
 
     // Fetch initial grid data from API
-    const {data, isLoading, isError, refetch} = useGridDataQuery();
+    const {data: gridData, isLoading: isGridDataLoading, isError: isGridDataError, refetch: refetchGridData} = useGridDataQuery();
+    const{data: grid, isLoading: isGridLoading, isError: isGridError, refetch: refetchGrid} = useGridQuery();
 
-  if(isError) {
-      return <div>Error fetching Grid Data... <a onClick={() => refetch()}>retry</a></div>
+    const {selectedUnit} = useContext(SelectedUnitContext);
+
+    function onRefetch() {
+        refetchGridData();
+        refetchGrid();
+    }
+
+  if(isGridDataError || isGridError) {
+      return <div>Error fetching Grid Data... <a onClick={() => onRefetch()}>retry</a></div>
   }
 
-  if(isLoading) {
+  if(isGridDataLoading|| isGridLoading) {
       return <p>Loading Grid Data...</p>;
   }
 
-  return (
-      <CurrentPlaybackContextProvider>
+    const maxStep = Math.max((gridData?.iterations?.length ?? 1) - 1, 0);
+    const gridSize = grid.grid[`${selectedUnit}`].length
+
+    return (
+      <CurrentPlaybackContextProvider maxStep={maxStep}>
         <SelectedUnitContextProvider>
         <div className="container">
             <div className="header">
                 <Header/>
             </div>
           <div className="grid">
-            <DataGrid data={data} gridSize={3} />
+            <DataGrid data={gridData} gridSize={gridSize} />
           </div>
           <div className="controls">
             <PlaybackControls/>
