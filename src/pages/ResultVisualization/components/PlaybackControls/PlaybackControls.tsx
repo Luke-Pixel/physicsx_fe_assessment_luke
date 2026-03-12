45c287c9908b26f8ecfa1e3e5cc67fd5966683fc
Luke Tyson <lukeool76@gmail.com>
2026-03-12 14:55:51 +0000

    add controls component

---
+++ src/pages/ResultVisualization/components/PlaybackControls/PlaybackControls.tsx
@@ -0,0 +1,25 @@
+import {useContext} from "react";
+import {CurrentPlaybackContext} from "../../store/CurrentPlaybackContext.ts";
+
+const PlaybackControls = () => {
+    const {state, dispatch} = useContext(CurrentPlaybackContext);
+
+    return (
+        <div>
+            <div style={{display: "flex", gap: "12px"}}>
+                <button type="button" onClick={() => dispatch({type: "PLAY"})}>
+                    Play
+                </button>
+                <button type="button" onClick={() => dispatch({type: "PAUSE"})}>
+                    Pause
+                </button>
+                <button type="button" onClick={() => dispatch({type: "RESET"})}>
+                    Reset
+                </button>
+            </div>
+            <p>{state.currentStep}</p>
+        </div>
+    );
+};
+
+export default PlaybackControls;
